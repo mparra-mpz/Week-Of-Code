dc2acc1b0cce10dd532d59e0acc5ee08e8e70100
Manuel Parra Z <mparra.mpz@gmail.com>
2017-12-21 00:51:46 -0300

    Add friendly message and a signal handler.

--- week-1/src/blink.c
+++ week-1/src/blink.c
@@ -12,22 +12,33 @@
 
 // Add the Wiring Pi library.
 #include <wiringPi.h>
+#include <stdio.h>
+#include <signal.h>
+
+static int tag = 1;
+// The SIGINT will break the loop.
+void stopSignal() {
+    tag = 0;
+}
 
 int main(void) {
+    printf("\nThis awesome application started.\n");
+    // Catch signal interruption SIGINT.
+    signal(SIGINT, stopSignal);
     // Wiring Pi library setup.
     wiringPiSetup();
     // Set pin GPIO 21 (pin 29) as output.
     pinMode(29, OUTPUT);
-    while(1) {
+    while(tag) {
         // Turn on the GPIO 21.
         digitalWrite(29, HIGH);
-        // Pause during 1 second.
-        delay(1000);
+        // Pause during 0.5 seconds.
+        delay(500);
         // Turn off the GPIO 21.
         digitalWrite(29,  LOW);
-        // Pause during 1 second.
-        delay (500) ;
+        // Pause during 0.5 seconds.
+        delay(500);
     }
-
-    return 0 ;
+    printf("\nThis awesome application finished.\n");
+    return 0;
 }

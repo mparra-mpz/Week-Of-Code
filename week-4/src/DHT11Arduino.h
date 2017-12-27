f58773705e6c65f7fce2705070f9b5c52b39bbf1
Manuel Parra <mparra.mpz@gmail.com>
2017-12-27 04:03:04 -0300

    Split arduino and raspberry implementation. Late in the clean code stage I will create a library for both shields.

---
+++ week-4/src/DHT11Arduino.h
@@ -0,0 +1,42 @@
+/*
+ * Author: Manuel Parra Z.
+ * Date: 23/12/2017
+ * License: MIT License
+ * Hardware:
+ *      - Arduino ONE R3.
+ *      - DHT11 DFRobot Sensor.
+ * Description: C++ library for DHT11 DFRobot Sensor.
+ */
+
+#ifndef DHT11_H
+#define DHT11_H
+
+#define DHT11_VERSION "0.0.1"
+
+#include "Arduino.h"
+
+enum DHT11_STATE {
+    OK,
+    ERROR_CHECKSUM,
+    ERROR_CONNECTION_L,
+    ERROR_CONNECTION_H,
+    ERROR_ACK_L,
+    ERROR_ACK_H
+};
+
+class DHT11 {
+
+    private:
+        int pin;
+        uint8_t bits[5];
+        int timeout;
+        float humidity;
+        float temperature;
+
+    public:
+        DHT11(int _pin);
+        DHT11_STATE read();
+        float getHumidity();
+        float getTemperature();
+};
+#endif
